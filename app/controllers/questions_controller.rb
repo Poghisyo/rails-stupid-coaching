79366c9d03561c9a9e13aa2c60c6e4331cecb6cf
Willem van Klinken <willem.v.klinken@gmail.com>
2018-02-19 17:12:14 +0100

    minor changes... major styling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,30 @@
 class QuestionsController < ApplicationController
-  def answer
-    @question = params[:query]
-  end
 
   def ask
-
   end
+
+  def answer
+    @question = params[:query]
+    if @question[-1] == '?'
+      @coach = 'angry-coach.jpg'
+      @answer = 'Silly question, get dressed and go to work!'
+
+    elsif @question == 'I am going to work'
+      @coach = 'happy-coach.jpg'
+      @answer = 'Great job! Get coding...'
+
+    else
+      @coach = 'angry-coach.jpg'
+      @answer = "I don't care, get dressed and go to work!"
+    end
+
+    # render layout: 'answer'
+  end
+
+  private
+
+  def question_params
+    params.require(:answer).permit(:query)
+  end
+
 end
